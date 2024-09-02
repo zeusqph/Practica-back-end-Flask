deef23fd5e6cebfca61ad10eef03bdda6bf6d2e3
zeus <zeushqp@gmail.com>
2024-09-01 21:31:05 -0500

    segundo avanze del proyecto

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from  flask import Flask
+from  flask import Flask , jsonify
 
 app = Flask(__name__)
 
@@ -6,7 +6,20 @@ from productos import productos
 
 @app.route("/")
 def entrada():
-    pass
+    return jsonify({"Mensaje":"Recivido"})
+
+@app.route("/productos")
+def GetProductos():
+    return jsonify({"Lista de Productos ": productos})
+
+@app.route("/productos/<string:nombre_producto>")
+def GetProducto(nombre_producto):
+    Producto_Encontrado =[producto for producto in productos if producto ["nombre"]== nombre_producto]
+    if len(Producto_Encontrado) == 0 :
+        return jsonify({"Error":"El producto no fue encontrado "}) , 404
+    
+
+    return  jsonify ({"producto":Producto_Encontrado[0]})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- productos.py
+++ productos.py
@@ -1,4 +1,14 @@
 productos = [
     {"nombre":"Laptop","Precio":2359,"Cantidad":27},
-    {"nombre":"Laptop","Precio":2359,"Cantidad":27}
+    {"nombre":"Teclado","Precio":840,"Cantidad":30},
+    {"nombre":"Mouse","Precio":2359,"Cantidad":50},
+    {"nombre":"Monitor","Precio":1899,"Cantidad":23},
+    {"nombre":"Iphone","Precio":8430,"Cantidad":15},
+    {"nombre":"Cargador de Lapto","Precio":259,"Cantidad":30},
+    {"nombre":"Teclado RGB","Precio":539,"Cantidad":11},
+    {"nombre":"Teclado RGB mecanico","Precio":850,"Cantidad":34},
+    {"nombre":"Xiomi E2","Precio":7899,"Cantidad":70},
+    {"nombre":"Camara","Precio":4459,"Cantidad":68},
+    {"nombre":"Laptop ASUS","Precio":5559,"Cantidad":55},
+    {"nombre":"Iphone 11 PRO MAX","Precio":7959,"Cantidad":30}
 ]
